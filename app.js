aeacf877b833986fc669c0d5fee7bbfbade64cce
“깃 <hy04730@naver.com>
2021-09-26 18:43:31 +0900

    로그인2

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     //localStorage에 username 저장
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+//localStorage에 저장된 username이 없다면 
+//로그인submit 보일 수 있게 hidden 클래스 가려줌
+//저장된 username이 있다면 Hello + username h1태그를 보여줌
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
