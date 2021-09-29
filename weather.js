555b7be9f2e22f15c081f297379672863ebe64f3
“깃 <hy04730@naver.com>
2021-09-29 14:18:48 +0900

    weather API

---
+++ weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "f1596d4ace391aeaf44396b0bc4b4f9a"
+
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live it ", lat, lon);
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response=>response
+        .json()).then(data=>{
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+    });
+}
+function onGeoError(){
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
