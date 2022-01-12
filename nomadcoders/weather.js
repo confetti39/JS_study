9888785985eea249fbdd418a1ae8c4c1f262ae0d
confetti39 <hy04730@naver.com>
2022-01-12 15:39:49 +0900

    폴더 변경

--- nomadcoders/weather.js
+++ nomadcoders/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "f1596d4ace391aeaf44396b0bc4b4f9a"
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live it ", lat, lon);
+    console.log("You live in ", lat, lon);
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(response => response
