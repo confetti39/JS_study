3d0c571490bd7298e3e7326149896774102e5351
“깃 <hy04730@naver.com>
2022-02-12 18:23:45 +0900

    boostcourse - super

--- boostcourse/JS-Object_Oriented_Programming/inheritance.js
+++ boostcourse/JS-Object_Oriented_Programming/inheritance.js
@@ -9,12 +9,22 @@ class Person {
     }
 }
 class PersonPlus extends Person {
+    constructor(name, first, second, third) {
+        super(name, first, second);
+        this.third = third;
+    }
+    sum() {
+        return super.sum() + this.third;
+    }
     avg() {
         return (this.first + this.second) / 2;
     }
 }
 
-const kim = new PersonPlus('kim', 10, 20);
+const kim = new PersonPlus('kim', 10, 20, 30);
 
 console.log('kim.sum()', kim.sum());
 console.log('kim.avg()', kim.avg());
+
+// super(): 부모 클래스의 생성자 호출
+// super.sum(): 부모 클래스
