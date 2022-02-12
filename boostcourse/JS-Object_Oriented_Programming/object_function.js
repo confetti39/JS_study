95f54c23f00fd054c79aea1cef923e675af5145b
“깃 <hy04730@naver.com>
2022-02-12 19:34:10 +0900

    boostcourse - bind

--- boostcourse/JS-Object_Oriented_Programming/object_function.js
+++ boostcourse/JS-Object_Oriented_Programming/object_function.js
@@ -9,3 +9,8 @@ function sum(prefix) {
 // call의 파라미터중 첫번째는 call 함수를 수행할 객체이며, 두번째부터 call 함수를 수행할 메소드의 파라미터이다.
 console.log("sum.call(kim)", sum.call(kim, "=> "));
 console.log("sum.call(lee)", sum.call(lee, ": "));
+const kimSum = sum.bind(kim, '-> ');
+console.log('kimSum()', kimSum());
+
+// 실행할 때 마다 어떤 함수의 this 값을 바꾸는, context를 바꾸는 함수 => call
+// call 처럼 실행할 때 마다 this를 변경하는 것이 아닌, 내부적으로 고정시켜 사용하고 싶다면 => bind
