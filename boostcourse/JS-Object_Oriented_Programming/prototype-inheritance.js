82a37a8bb8d62a23a33914734ae4aece0fc5571d
“깃 <hy04730@naver.com>
2022-02-12 18:48:35 +0900

    boostcourse - Object.create

--- boostcourse/JS-Object_Oriented_Programming/prototype-inheritance.js
+++ boostcourse/JS-Object_Oriented_Programming/prototype-inheritance.js
@@ -1,7 +1,12 @@
 const superObj = { superVal: 'super' }
-const subObj = { subVal: 'sub' }
+// const subObj = { subVal: 'sub' }
+// subObj.__proto__ = superObj;
 
-subObj.__proto__ = superObj; //prototype, __proto__ 각각 다른 기능임
+const subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+
+debugger;
+
 console.log('subObj.subVal => ', subObj.subVal);
 console.log('subObj.superVal => ', subObj.superVal);
 subObj.superVal = 'sub';
